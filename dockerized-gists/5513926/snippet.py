f29744bf7d56eedf9a105a25eb7739fdb0d9d800
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-02-18 14:37:36 +0100

    point DOIs to preferred resolver

--- dockerized-gists/5513926/snippet.py
+++ dockerized-gists/5513926/snippet.py
@@ -5,7 +5,7 @@ def doi2bib(doi):
   Return a bibTeX string of metadata for a given DOI.
   """
 
-  url = "http://dx.doi.org/" + doi
+  url = "https://doi.org/" + doi
 
   headers = {"accept": "application/x-bibtex"}
   r = requests.get(url, headers = headers)
